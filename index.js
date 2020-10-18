550bbba656032fec9e23f2f0fb5c3cd0678de8e7
Axel <6265267+Akke@users.noreply.github.com>
2020-10-18 17:58:49 +0200

    Adding event emitter for

--- index.js
+++ index.js
@@ -34,7 +34,9 @@ export default class TrackSpotify extends BaseModule {
         const trackList = [];
 
         data.tracks.items.forEach((item) => trackList.push(new SpotifyTrack(this._m, isAlbum ? item : item.track, isAlbum ? data.images[0].url : null)));
-
+    
+        this._m.emit(isAlbum ? 'albumPlayed' : 'playlistPlayed');
+        
         return trackList;
     }
 
